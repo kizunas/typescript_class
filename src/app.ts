8fcd03330dbacd91cfe3b0f3528574bc015634c3
kizunas <8s6s7e@gmail.com>
2024-03-09 16:56:10 +0900

    クラスの書き方と初期化

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+//頭文字を大文字にする。名前は任意
+class Department {
+  name: string; // キーの名前を定義しているだけ。イコールで初期値を設定できる。
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const accounting = new Department('Accounting'); // この初期化の処理のときのみconstructorが呼ばれる。
+console.log(accounting);
+// 出力では　name: 'Accounting'と出力される。
