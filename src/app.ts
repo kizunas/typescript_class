42fce213cd7a2f28c647b9c8b85008822bfa1a00
kizunas <8s6s7e@gmail.com>
2024-03-16 16:59:35 +0900

    constructor記述のショートカット

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 class Department {
-  name: string; // defaultでpublicが当てられるので記述しなくても良い。クラス外部から変更できる。
+  // name: string; // defaultでpublicが当てられるので記述しなくても良い。クラス外部から変更できる。
   private employees: string[] = []; // privateをつけるとクラスと外から変更ができない。すなわちクラス内からの変更のみを許可
-
-  constructor(n: string) {
-    this.name = n;
+  // private id: string;
+  constructor(private id: string, public name: string) { // この記述ではpublicは省略できない。また、引数とプロパティの名前を一致させる必要がる。nではなくnameのように。
+    // this.id = id // このように毎回宣言するのは大変なので上記のようにきじゅつできる。
+    // this.name = n;
   }
 
   describe() { // methods
-    console.log('Department: ' + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -20,10 +21,12 @@ class Department {
   }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting'); // constructorでidが追加されたのでidを渡そう
 
 accounting.addEmployee('Max')
 accounting.addEmployee('Manu')
+
+accounting.name = 'NEW NAME'
 
 accounting.describe();
 accounting.printEmployeeInformation();
