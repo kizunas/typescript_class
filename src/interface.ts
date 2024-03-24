cc999ed2b160d2d82c14fb94d659a40dd580b455
kizunas <8s6s7e@gmail.com>
2024-03-24 15:24:23 +0900

    interface2

--- src/interface.ts
+++ src/interface.ts
@@ -1,21 +1,29 @@
 // interfaceというキーワードはtypescriptにしか存在しない
 // objectがどのような形であるかを定義するためのもの
 // オリジナルの型を定義するもの
-interface Person {
+// interfaceはオブジェクトだけ。typeは他にも定義が可能である。
+interface Greetable {
   name: string; // 具体的な値やdefault値などを設定することはできない。
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person // Personという型を適用することができる
+// Greetableというインターフェースに従って実装されるということを伝える(これをインターっフェースを実装するという)
+class Person implements Greetable { // 一つのクラスには複数のインターフェースを実装できる（継承との違い）。カンマ(,)でつなげてインターフェースを記述すればよい
+ name: string;
+ age = 30;
+ constructor(n: string) {
+  this.name = n;
+ }
 
-user1 = { // user1はオブジェクトでなければならない。
-  name: 'Max',
-  age: 30,
-  greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
-  }
+ greet(phrase: string) {
+  console.log(phrase + ' ' + this.name); 
+ }
+
+ // インターフェース以外の項目を追加することも可能。
 }
+let user1: Greetable // インターフェースは型として利用することが可能
 
-user1.greet('Hello I am')
+user1 = new Person('Max'); // PersonクラスはGreetableのインターフェースを実装しているため。
+
+user1.greet('Hello I am');
