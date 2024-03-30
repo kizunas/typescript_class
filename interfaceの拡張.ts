1a8f0002951719928e60bffd082db4d15936233a
kizunas <8s6s7e@gmail.com>
2024-03-30 13:57:52 +0900

    interfaceの拡張1

---
+++ interfaceの拡張.ts
@@ -0,0 +1,25 @@
+interface Named {
+  readonly name: string;
+}
+
+interface Greetable {
+  greet(phrase: string): void;
+}
+
+
+class Person implements Greetable, Named {
+ age = 30;
+ constructor(n: string) {
+  this.name = n;
+ }
+
+ greet(phrase: string) {
+  console.log(phrase + ' ' + this.name); 
+ }
+
+ 
+}
+let user1: Greetable
+user1 = new Person('Max');
+
+user1.greet('Hello I am');
